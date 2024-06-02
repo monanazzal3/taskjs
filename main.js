768d056ec593702e88742bf979a292fe356f9414
monanazzal3 <163445761+monanazzal3@users.noreply.github.com>
2024-06-02 22:34:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".userProfile h2").textContent="hello mona";
+
+console.log(document.querySelector(".userProfile").textContent="hello mona");
+console.log(document.querySelector(".userProfile p").classList.add("userInfo"))
